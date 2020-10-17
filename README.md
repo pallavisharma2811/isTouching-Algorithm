26dafe4513b2386532b39c4a96fe17a0f9fadc71
Pallavi <61927103+pallavisharma2811@users.noreply.github.com>
2020-10-17 16:05:13 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# isTouching-Algorithm
+# collisionAlgorithm
+Algorithm to detect collision between two objects
---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect, movingRect;
+var trex, obstacle;
+var ball, paddle;
+var object1, object2;
+
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+// we have passed two arguments to our function while calling it
+// throw argument from function call
+  if(isTouching(fixedRect, movingRect)){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }else{
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+  
+  drawSprites();
+}
+
